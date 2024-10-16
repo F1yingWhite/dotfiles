e0d29ffd66f25412177f8c9ac88697c9fabe3478
F1yingWhite <81723334@qq.com>
2024-10-16 16:26:55 +0800

    fzf支持图片预览

--- zsh/fzf_preview.sh
+++ zsh/fzf_preview.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $(file --mime-type "$1") =~ image/* || $(file --mime-type "$1") =~ video/* ]]; then
-  kitten icat --clear --transfer-mode=memory --stdin=no --place=${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}@20x1 "$1"
+  kitten icat --clear --transfer-mode=memory --stdin=no --place=${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}@0x0 "$1"
 else
   bat --color=always --line-range :500 "$1"
 fi
